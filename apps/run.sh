09bd43f8bd6179af5e64fc7889fd906f4fe1059a
woooooooooooook <nubiz@naver.com>
2025-02-10 01:25:49 +0900

    1

--- apps/run.sh
+++ apps/run.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/with-contenv bashio
+set -e
 
 # 웹 애플리케이션 실행 (개발 모드)
 FLASK_ENV=development 
 FLASK_DEBUG=1 
-export SUPERVISOR_TOKEN=$SUPERVISOR_TOKEN
 
-exec python3 app.py
+# 최대 대기 시간 (초 단위)
+TIMEOUT=30
+
+# SUPERVISOR_TOKEN 환경변수가 설정될 때까지 대기
+while [ $TIMEOUT -gt 0 ] && [ -z "$SUPERVISOR_TOKEN" ]; do
+  echo "Waiting for SUPERVISOR_TOKEN environment variable to be set..."
+  sleep 1
+  TIMEOUT=$(( TIMEOUT - 1 ))
+done
+
+# 환경변수가 없으면 에러 처리
+if [ -z "$SUPERVISOR_TOKEN" ]; then
+  echo "Error: SUPERVISOR_TOKEN environment variable not set after waiting."
+  exit 1
+fi
+
+echo "SUPERVISOR_TOKEN is set, starting app..."
+exec python app.py
