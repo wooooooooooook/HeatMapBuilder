1810d10ed94aeae94956c1369b407f33b53815f7
woooooooooooook <nubiz@naver.com>
2025-02-10 08:04:58 +0900

    tz

--- apps/run.sh
+++ apps/run.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/with-contenv bashio
-set -e
 
 # 웹 애플리케이션 실행 (개발 모드)
 FLASK_ENV=development 
 FLASK_DEBUG=1 
 
-export SUPERVISOR_TOKEN=$(bashio::supervisor.token)
+TZ=$(bashio::supervisor.timezone)
+if [ -n "$TZ" ]; then
+    echo Timezone: $TZ
+    cp /usr/share/zoneinfo/$TZ /etc/localtime
+    echo $TZ > /etc/timezone
+fi
 
 exec python app.py
