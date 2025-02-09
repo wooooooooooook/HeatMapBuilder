7507b9d4124f9392d7778eedde51e3a138aa0356
woooooooooooook <nubiz@naver.com>
2025-02-10 01:17:40 +0900

    1

--- apps/run.sh
+++ apps/run.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/with-contenv bashio
-# 필요한 디렉토리 생성
-mkdir -p /data
-mkdir -p /media
+
 # 웹 애플리케이션 실행 (개발 모드)
 FLASK_ENV=development 
 FLASK_DEBUG=1 
+export SUPERVISOR_TOKEN=$SUPERVISOR_TOKEN
 
-python3 app.py
+exec python3 app.py
